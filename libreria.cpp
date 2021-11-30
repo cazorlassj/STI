2744671b8197289ade67faeb99dcfe5026f7e011
cazorla <51689868+cazorlassj@users.noreply.github.com>
2021-11-29 22:37:13 -0300

    Creacion del header libreria
    Creacion del header.
    La musica y el texto ahora funcionan al mismo tiempo

--- libreria.cpp
+++ libreria.cpp
@@ -1,107 +1,134 @@
 #include <windows.h>
 #include <stdio.h>
 #include <time.h>
+#include "libreria.h"
+#include <thread>
 
-
-chau(){
-	int dou = 261, re = 294, mi = 330, fa = 349, sol = 392, la = 440, si = 494, time = 370, x = 80, y = 200;
-	system("pause");
+void text(){
+	int dou = 261, re = 294, mi = 330, fa = 349, sol = 392, la = 440, si = 494, time = 370, x = 80;
 	printf("Gracias por usar el programa!");
-	Sleep(200);
-	//mi fa mi re mi do 
-	Beep(mi, time);
+	Sleep(300);
 	system("cls");
 	printf("o");
 	Sleep(x);
-	Beep(fa, time);
 	system("cls");
 	printf("do");
 	Sleep(x);
-	Beep(mi, time);
 	system("cls");
 	printf("edo");
 	Sleep(x);
-	Beep(re, time);
 	system("cls");
 	printf("bedo");Sleep(x);
-	Beep(mi, time);
 	system("cls");
 	printf("obedo");Sleep(x);
-	Beep(re, time);
 	system("cls");
 	printf("cobedo");Sleep(x);
-	Beep(dou, time);
 	system("cls");
 	printf("scobedo");Sleep(x);
-	//do si la do si si re do do
-	Beep(dou, time);
 	system("cls");
 	printf("Escobedo");Sleep(x);
-	Beep(si, time);
 	system("cls");
 	printf(" Escobedo");Sleep(x);
-	Beep(la, time);
 	system("cls");
 	printf("n Escobedo");Sleep(x);
-	Beep(dou, time);
 	system("cls");
 	printf("on Escobedo");Sleep(x);
-	Beep(si, time);
 	system("cls");
 	printf("con Escobedo");Sleep(x);
-	Beep(si, time);
 	system("cls");
 	printf("acon Escobedo");Sleep(x);
-	Beep(re, time);
 	system("cls");
 	printf("hacon Escobedo");Sleep(x);
-	Beep(dou, time);
 	system("cls");
 	printf("Chacon Escobedo");Sleep(x);
-	Beep(dou, time);
 	system("cls");
 	printf(" Chacon Escobedo");Sleep(x);
-	//mi fa sol la sol fa re do do
-	Beep(mi, time);
 	system("cls");
 	printf("o Chacon Escobedo");Sleep(x);
-	Beep(fa, time);system("cls");
+	system("cls");
 	printf("ro Chacon Escobedo");Sleep(x);
-	Beep(sol, time);system("cls");
+	system("cls");
 	printf("tro Chacon Escobedo");Sleep(x);
-	Beep(la, time);system("cls");
+	system("cls");
 	printf("stro Chacon Escobedo");Sleep(x);
-	Beep(sol, time);system("cls");
+	system("cls");
 	printf("astro Chacon Escobedo");Sleep(x);
-	Beep(fa, time);system("cls");
+	system("cls");
 	printf("Castro Chacon Escobedo");Sleep(x);
-	Beep(re, time);system("cls");
+	system("cls");
 	printf(" Castro Chacon Escobedo");Sleep(x);
-	Beep(dou, time);system("cls");
+	system("cls");
 	printf("a Castro Chacon Escobedo");Sleep(x);
-	Beep(dou, time);system("cls");
+	system("cls");
 	printf("la Castro Chacon Escobedo");Sleep(x);
-	//do si la do si si re do do
-	Beep(dou, time);system("cls");
+	system("cls");
 	printf("rla Castro Chacon Escobedo");Sleep(x);
-	Beep(si, time);system("cls");
+	system("cls");
 	printf("orla Castro Chacon Escobedo");Sleep(x);
-	Beep(la, time);system("cls");
+	system("cls");
 	printf("zorla Castro Chacon Escobedo");Sleep(x);
-	Beep(dou, time);system("cls");
-	printf("zorla Castro Chacon Escobedo");Sleep(x);
-	Beep(si, time);system("cls");
+    system("cls");
 	printf("azorla Castro Chacon Escobedo");Sleep(x);
-	Beep(si, time);system("cls");
-	printf("\033[1;31m");
-	printf("Cazorla Castro Chacon Escobedo");
+	for(int i = 0; i < 24; i++){
+		system("cls");
+		printf("\033[1;31m");
+		printf("Cazorla Castro Chacon Escobedo");Sleep(100);
+		system("cls");
+		printf("\033[0;34m");
+		printf("Cazorla Castro Chacon Escobedo");Sleep(100);
+		system("cls");
+		printf("\033[0;35m");
+		printf("Cazorla Castro Chacon Escobedo");Sleep(100);
+	}
+}
+
+void musica(){
+	int dou = 261, re = 294, mi = 330, fa = 349, sol = 392, la = 440, si = 494, time = 400, x = 80, y = 200;
+	//mi fa mi re mi do 
+	Beep(mi, time);
+	Beep(fa, time);
+	Beep(mi, time);
 	Beep(re, time);
-	system("cls");
-	printf("\033[0;34m");
-	printf("Cazorla Castro Chacon Escobedo");
+	Beep(mi, time);
+	Beep(re, time);
 	Beep(dou, time);
-	system("cls");
-	printf("\033[0;35m");
-	printf("Cazorla Castro Chacon Escobedo");
+	//do si la do si si re do do
 	Beep(dou, time);
+	Beep(si, time);
+	Beep(la, time);
+	Beep(dou, time);
+	Beep(si, time);
+	Beep(si, time);
+	Beep(re, time);
+	Beep(dou, time);
+	Beep(dou, time);
+	//mi fa sol la sol fa re do do
+	Beep(mi, time);
+	Beep(fa, time); 
+	Beep(sol, time); 
+	Beep(la, time); 
+	Beep(sol, time); 
+	Beep(fa, time); 
+	Beep(re, time); 
+	Beep(dou, time); 
+	Beep(dou, time); 
+	//do si la do si si re do do
+	Beep(dou, time); 
+	Beep(si, time); 
+	Beep(la, time); 
+	Beep(dou, time); 
+	Beep(si, time); 
+	Beep(si, time); 
+	Beep(re, time);
+	Beep(dou, time);
+	Beep(dou, time);
+}
+
+void chau(){
+	using namespace std;
+	std::thread x(musica);
+  	std::thread y(text);
+  	x.join();
+  	y.join();
+  	exit(1);
 }
---
+++ libreria.h
@@ -0,0 +1,41 @@
+#ifndef _LIBRERIA
+#define _LIBRERIA
+struct Fecha{
+	int dia, mes, anio;	
+};
+
+struct Usuarios{
+	char user[10];
+	char pw[10];
+	char apenom[60];
+};
+
+struct Profesionales{
+	char apenom[60];
+	int idp;
+	int dni;
+	char tel[25];
+};
+
+struct Cliente{
+	char apenom[60];
+	char dom[60];
+	int dni;
+	char localidad[60];
+	Fecha fec;
+	float peso;
+	char tel[25];
+};
+
+struct Turnos{
+	int idp;
+	Fecha fec;
+	int dni;
+	char detalle[380];
+};
+
+void chau();
+void musica();
+void text();
+#include "libreria.cpp"
+#endif
